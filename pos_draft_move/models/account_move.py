b6b85a0d2a760e5101c3c7df2b1cc807a208cb86
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-11-06 15:23:38 +0100

    [MIG] pos_draft_move: Migration to 12.0

--- pos_draft_move/models/account_move.py
+++ pos_draft_move/models/account_move.py
@@ -1,9 +1,8 @@
-# coding: utf-8
 # Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
-from openerp import api, models
+from odoo import api, models
 
 
 _logger = logging.getLogger(__name__)
--- pos_draft_move/models/pos_session.py
+++ pos_draft_move/models/pos_session.py
@@ -1,8 +1,7 @@
-# coding: utf-8
 # Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PosSession(models.Model):
