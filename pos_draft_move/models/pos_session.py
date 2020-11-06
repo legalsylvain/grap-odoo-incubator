685a1dba6431082a22c4418119e4ec66ad71a91b
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-11-06 15:36:21 +0100

    [WIP]

--- pos_draft_move/models/pos_session.py
+++ pos_draft_move/models/pos_session.py
@@ -1,18 +1,14 @@
 # Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class PosSession(models.Model):
     _inherit = "pos.session"
 
-    generate_draft_moves = fields.Boolean(
-        string="Generate Draft Moves", default=False
-    )
-
     @api.multi
-    def wkf_action_close(self):
+    def action_pos_session_validate_draft(self):
         super(
             PosSession, self.with_context(pos_draft_move=True)
-        ).wkf_action_close()
+        ).action_pos_session_validate()
